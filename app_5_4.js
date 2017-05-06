cc4af70211a1eb23c3cf0218e68dafce13a37c63
kshaw661 <risk.always@gmail.com>
2017-05-06 09:43:51 +0530

    Add files via upload

---
+++ app_5_4.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Assignment 5.4 : JavaScript objects and arrays. JSON</title>
+		
+		<!-- Internal Style Example -->
+		<style>body{margin:5px; padding:5px;}</style>
+	</head>
+	<body>
+		
+	</body>
+	<!-- External Javascript -->
+		<script type="text/javascript" src="app_5_4.js"></script>
+		<!-- End -->
+</html> 
