3b12bcd4a73e6aae62e45eb94e0e99f75259ab9f
kshaw661 <risk.always@gmail.com>
2017-05-06 09:44:41 +0530

    Update app_5_4.js

--- app_5_4.js
+++ app_5_4.js
@@ -1,15 +1,64 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Assignment 5.4 : JavaScript objects and arrays. JSON</title>
-		
-		<!-- Internal Style Example -->
-		<style>body{margin:5px; padding:5px;}</style>
-	</head>
-	<body>
-		
-	</body>
-	<!-- External Javascript -->
-		<script type="text/javascript" src="app_5_4.js"></script>
-		<!-- End -->
-</html> 
+var obj = [
+	{
+					name: "Kunal Shaw",
+					age: 32,
+					Salary:45599,
+					address:{
+							city:'Kolkata',
+							State:'West Bengal',
+							pincode:743135,
+							
+					},
+				},
+	{
+		name: "Nick Shaw",
+		age: 30,
+		Salary:65999,
+		address:{
+				city:'Purulia',
+				State:'West Bengal',
+				pincode:743139,
+				
+		},
+	},
+	{
+		name: "Kumar Shaw",
+		age: 29,
+		Salary:69999,
+		address:{
+				city:'Salt Lake',
+				State:'West Bengal',
+				pincode:110096,
+				
+		},
+	},
+	{
+		name: "Kunal Shaw",
+		age: 32,
+		Salary:45599,
+		address:{
+				city:'Delhi',
+				State:'Delhi',
+				pincode:110096,
+				
+		},
+	},
+	{
+		name: "Nick",
+		age: 33,
+		Salary:75599,
+		address:{
+				city:'Kuchupuri',
+				State:'Warangal',
+				pincode:743135,
+				
+		},
+	},
+];
+console.log('Employeed Name : '+obj[0]['name']);
+console.log('Employeed Age : '+obj[0]['age']);
+console.log('Employeed Salary : '+obj[0]['Salary']);
+console.log('Employeed City : '+obj[0]['address']['city']);
+console.log('Employeed State : '+obj[0]['address']['State']);
+console.log('Employeed Pincode : '+obj[0]['address']['pincode']);
+
